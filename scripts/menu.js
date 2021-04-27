1c5c8c31a213ef0684cdd36b6f2b7c5cf39c25d7
GabrielaTicu <78794186+GabrielaTicu@users.noreply.github.com>
2021-04-28 02:49:04 +0300

    Add files via upload

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,13 @@
 function show() {
-    document.getElementById('text1').style.display='block';
-    document.getElementById('hide1').style.display='inline';
-    document.getElementById('show1').style.display='none'; 
+    document.getElementById('expander').style.display='block';
+    document.getElementById('hide').style.display='inline';
+    document.getElementById('show').style.display='none'; 
     return false;
 }
 
 function hide() {
-    document.getElementById('text1').style.display='none';
-    document.getElementById('hide1').style.display='none';
-    document.getElementById('show1').style.display='inline'; 
+    document.getElementById('expander').style.display='none';
+    document.getElementById('hide').style.display='none';
+    document.getElementById('show').style.display='inline'; 
     return false;
 }
